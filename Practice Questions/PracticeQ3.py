04e2f72bd9f916874c03509da0cf24764637462d
Katakam Sai Deepika <katakamsaideepika@gmail.com>
2024-07-18 22:05:42 +0530

    Adding new

---
+++ Practice Questions/PracticeQ3.py
@@ -0,0 +1,5 @@
+# input 2 float numbers and print their average.
+num1 = float(input("enter first float number : "))
+num2 = float(input("Enter second float number : "))
+avg = (num1+num2)/2
+print(avg)
---
+++ PracticeQ2.py
@@ -0,0 +1,5 @@
+# Write a program to print area of a square 
+side = float(input("Enter side of square : "))
+area = side * side
+# area = side ** 2
+print("Area of square : ",area)
