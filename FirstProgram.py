2f3d444a4e984c6b0fa00707b951f6e5d1d67358
Katakam Sai Deepika <katakamsaideepika@gmail.com>
2024-07-18 11:02:33 +0530

    Add of intro file in repo

---
+++ FirstProgram.py
@@ -0,0 +1,79 @@
+# Python is Case Sensitive Language.
+
+print("Hello World!")
+print("Katakam Sai Deepika")
+print("Katakam Sai Deepika is my name.","My age is 20.")
+print(24)
+print(45+5)
+# Variables 
+name = "Deepika"
+print("My name is : ",name)
+name = "Manoj"
+print("My name is : ",name)
+price = 500
+print(price)
+# To know the data type of the variable.
+print(type(name))
+print(type(price))
+price2 = price
+print(price2)
+price = 650
+print(price)
+print(price2)
+# Data types - Integer, String, Float, Boolean, None
+# None - when we dont want to store any data in variable
+name1 = "sk"
+name2 = 'sk'
+name3 = '''sk'''
+print(name1)
+print(name2)
+print(name3)
+a = None
+print(type(a))
+print(a)
+
+# Print sum of two numbers 
+a = 2
+b = 4
+print(a+b)
+# c = int(input())
+# d = int(input())
+# sum = c+d
+# print(sum)
+
+# # Simple Calculator :
+# var1 = int(input())
+# var2 = int(input())
+# sum = var1+var2
+# diff = var1 - var2
+# mul = var1*var2
+# div = var1 / var2
+# floor_div = var1// var2
+# mod = var1 % var2
+# exp = var1 ** var2
+# print(sum)
+# print(diff)
+# print(mul)
+# print(div)
+# print(floor_div)
+# print(mod)
+# print(exp)
+
+# Tokens - Punctuators - (),{},@,[],# --- used to organize sentence structure.
+# Python is a typed language and it is a implicit language (no need to specify type while declaring variable)
+# Str and numbers can work with * - repitition operator
+# Str and Str work with + - concatenation operator
+str = "@"
+a,b = "2",3
+print((a+str)*b)
+print(2*str*3)
+a = 7
+print(a/b)
+print(a//b)
+var3 = 4.6
+var4 = 5
+var5 = -2
+print(var4%var5) # when denominator has a negative sign then output of modulo has a negative sign.
+"""
+This is a multi line comment, written using triple double qoutes.""" 
+
