74a8303954db5935911604ba3a3b6c20d8fd5d79
Katakam Sai Deepika <katakamsaideepika@gmail.com>
2024-07-18 22:59:56 +0530

    Adding new strings folder

---
+++ Strings and Conditional Statements/Str1.py
@@ -0,0 +1,19 @@
+str1 = "This is a String"
+str2 = 'Katakam Sai Deepika'
+str3 = """This is a string. \nWe are creating it in python"""
+str4 = """This is a string. \tWe are creating it in python"""
+print(str1)
+print(str2)
+print(str3)
+print(str4)
+
+# Concatenation - combining two strings using '+' sign in python
+s1 = "Katakam"
+s2 = " Sai Deepika"
+print(s1+ s2)
+print(len(s1))
+print(len(s2))
+
+# Length of a string using len(str)
+name = "Deepika"
+print(len(name))  # len() -- built in function in python.
