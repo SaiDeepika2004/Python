b116a25606df00a542b52aaa284b87465f5e7710
Katakam Sai Deepika <katakamsaideepika@gmail.com>
2024-07-18 22:13:27 +0530

    Adding new practice Question into folder.

---
+++ Practice Questions/PracticeQ4.py
@@ -0,0 +1,9 @@
+# For 2 integers a,b if a is grater than b print True else False
+a = int(input("a : "))
+b = int(input("b : "))
+# if a >= b:
+#     print("True")
+# else:
+#     print("False")
+res = "True" if a >= b else "False"
+print(res)
