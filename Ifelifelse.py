62618a51edeec842cf75212031859ce404e0143f
Katakam Sai Deepika <katakamsaideepika@gmail.com>
2024-07-18 11:05:04 +0530

    Adding new files

---
+++ Ifelifelse.py
@@ -0,0 +1,13 @@
+num = int(input())
+if(num >= 70):
+    print("Grade A")
+elif(num >=60):
+    print("Grade B")
+elif(num >=50):
+    print("Grade C")
+elif(num >=40):
+    print("Grade D")
+elif(num >=35):
+    print("Grade E")
+else:
+    print("Fail")
---
+++ TrafficSignal.py
@@ -0,0 +1,9 @@
+light = input()
+if light == "Red":
+    print("Stop")
+elif light == "Green":
+    print("Go")
+elif light=="Yellow":
+    print("Wait")
+elif light=="Blue" or light=="White":
+    print("Light is broken.")
